6e9e0ee606c5dffcf25f4dddf0427f24dedfda3c
Klyona Dyck <klyona.dyck@gmail.com>
2026-02-10 19:25:22 +0500

    New commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE_PATH = "https://3a5c4438-24b8-4d94-b8e1-eafd260b4609.serverhub.praktikum-services.ru" #URL веб-сервиса
+CREATE_ORDER_PATH = "/api/v1/orders" # Путь до создания заказа
+TRACK_ORDERS_PATH = "/api/v1/orders/track?t=" # Путь получения заказа по треку заказа
