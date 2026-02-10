0d6953778f93b8130032c23b39b256b01b3ec998
Klyona Dyck <klyona.dyck@gmail.com>
2026-02-10 23:38:01 +0500

    new changes

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE_PATH = "https://3a5c4438-24b8-4d94-b8e1-eafd260b4609.serverhub.praktikum-services.ru" #URL веб-сервиса
+URL_SERVICE_PATH = "https://a2dcb0ed-f115-4052-b364-b6b6210ec116.serverhub.praktikum-services.ru/" #URL веб-сервиса
 CREATE_ORDER_PATH = "/api/v1/orders" # Путь до создания заказа
 TRACK_ORDERS_PATH = "/api/v1/orders/track?t=" # Путь получения заказа по треку заказа
