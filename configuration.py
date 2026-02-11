c1739c196398ad7b9e89876145739c190e29d6a2
Klyona Dyck <klyona.dyck@gmail.com>
2026-02-11 12:05:18 +0500

    lastlastlast

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE_PATH = "https://a2dcb0ed-f115-4052-b364-b6b6210ec116.serverhub.praktikum-services.ru/" #URL веб-сервиса
+URL_SERVICE_PATH = "https://b80f510a-e61b-41a0-b5e4-90d5e5ae11ce.serverhub.praktikum-services.ru" #URL веб-сервиса
 CREATE_ORDER_PATH = "/api/v1/orders" # Путь до создания заказа
 TRACK_ORDERS_PATH = "/api/v1/orders/track?t=" # Путь получения заказа по треку заказа
--- create_order_test.py
+++ create_order_test.py
@@ -3,7 +3,7 @@ import data
 import sender_stand_request
 
 def test_status_code_for_order_200():
-    order_response = sender_stand_request.create_order_test(data.order_body)
+    order_response = sender_stand_request.create_order(data.order_body)
     track = order_response.json()["track"]
     get_response = sender_stand_request.get_order_by_track(track)
     assert get_response.status_code == 200
